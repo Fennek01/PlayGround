6d67ae1f064f1ad83e9399bab87cb5529b4faac4
Fennek01 <110638920+Fennek01@users.noreply.github.com>
2022-11-22 21:55:05 -0300

    Upgrade 3.0

--- PlayGround/src/app/pages/content/conteudo-customizado/conteudo-customizado.component.ts
+++ PlayGround/src/app/pages/content/conteudo-customizado/conteudo-customizado.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: "Custom",
-  templateUrl: './conteudo-customizado.component.html'
+  templateUrl: './conteudo-customizado.component.html',
+  styleUrls: ['./conteudo-customizado.component.css']
+  
 })
 export class ConteudoCustomizadoComponent {
+
+  valor = 0;
+
+  abrir() {
+    if (this.valor == 0) {
+      this.valor = 1;
+      return this.valor;
+    } else  {
+      this.valor = 0;
+      return this.valor;
+    }
+  }
+
+  fechar() {
+      this.valor = 1;
+      return this.valor;
+  }
 
 }
